217f4f627eef97e2589ba18b953be6a1f667f489
Ubuntu <ubuntu@ip-172-31-3-183.eu-central-1.compute.internal>
2020-12-03 11:09:52 +0000

    fix

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -10,20 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlcat(char *dest, char *src, unsigned int nb)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb)
 {
+	unsigned int	src_len;
 	unsigned int	dest_len;
 	unsigned int	index;
 
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
 	dest_len = 0;
 	while (dest[dest_len])
 		dest_len++;
 	index = 0;
-	while (src[index] && index < nb)
+	while (src[index] && (index < nb - 1))
 	{
 		dest[dest_len + index] = src[index];
 		index++;
 	}
 	dest[dest_len + index] = '\0';
-	return (dest);
+	return (src_lne + dest_len);
 }
