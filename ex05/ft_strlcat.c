2bc5dad82afc053508977db0d46eb2f43607f5ce
Joao pedro Ceia <jceia@c2r13s1.42lisboa.com>
2020-12-03 16:06:47 +0000

    fix

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:53:29 by jceia             #+#    #+#             */
-/*   Updated: 2020/11/30 22:07:25 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/03 15:48:50 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb)
 	unsigned int	index;
 
 	src_len = 0;
-	while (src[src_len])
+	while (src[src_len] && src_len < nb)
 		src_len++;
 	dest_len = 0;
 	while (dest[dest_len])
@@ -29,5 +29,5 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb)
 		index++;
 	}
 	dest[dest_len + index] = '\0';
-	return (src_len);
+	return (src_len + dest_len);
 }
