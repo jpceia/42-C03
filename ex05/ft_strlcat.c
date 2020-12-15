9520277388c254a3a0526aabe98b2d90f580a272
Joao pedro Ceia <jceia@c2r14s4.42lisboa.com>
2020-12-15 13:18:57 +0000

    ft_strlcat

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,28 +6,46 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:53:29 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/03 15:48:50 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/15 13:17:27 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb)
+unsigned int	ft_strlen(char *str)
 {
-	unsigned int	src_len;
-	unsigned int	dest_len;
 	unsigned int	index;
 
-	src_len = 0;
-	while (src[src_len] && src_len < nb)
-		src_len++;
-	dest_len = 0;
-	while (dest[dest_len])
-		dest_len++;
 	index = 0;
-	while (src[index] && (dest_len + index + 1 < nb))
-	{
-		dest[dest_len + index] = src[index];
+	while (str[index])
 		index++;
-	}
-	dest[dest_len + index] = '\0';
-	return (src_len + dest_len);
+	return (index);
 }
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb)
+{
+	char			*dest_ptr;
+	char			*src_start;
+	unsigned int	dest_len;
+	unsigned int	remaing;
+
+	dest_ptr = dest;
+	src_start = src;
+	remaing = nb;
+	while (remaing-- != 0 && *dest_ptr != '\0')
+		dest_ptr++;
+	dest_len = dest_ptr - dest;
+	remaing = nb - dest_len;
+	if (remaing == 0)
+		return (dest_len + ft_strlen(src));
+	while (*src != '\0')
+	{
+		if (remaing > 1)
+		{
+			*dest_ptr++ = *src;
+			remaing--;
+		}
+		src++;
+	}
+	*dest_ptr = '\0';
+	return (dest_len + (src - src_start));
+}
+
