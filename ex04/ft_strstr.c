6cc267974cc171a56522ed5bc1c28ee7bc250270
Joao pedro Ceia <jceia@c2r14s4.42lisboa.com>
2020-12-15 12:50:54 +0000

    fixed strstr

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,28 +6,30 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:52:36 by jceia             #+#    #+#             */
-/*   Updated: 2020/11/30 20:56:49 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/15 12:48:58 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	unsigned int	index;
+	char	*str_cpy;
+	char	*to_find_cpy;
 
-	if (!to_find)
+	if (*to_find == '\0')
 		return (str);
-	while (*str)
+	str_cpy = str;
+	to_find_cpy = to_find;
+	while (1)
 	{
-		index = 0;
-		while (to_find[index] != 0)
-		{
-			if (to_find[index] != str[index])
-				break ;
-			index++;
-		}
-		if (to_find[index] == 0)
-			return (str);
-		str++;
+		if (*to_find_cpy == '\0')
+			return (str_cpy - (to_find_cpy - to_find));
+		if (*str_cpy == *to_find_cpy)
+			to_find_cpy++;
+		else
+			to_find_cpy = to_find;
+		if (str_cpy == '\0')
+			break ;
+		str_cpy++;
 	}
 	return (0);
 }
